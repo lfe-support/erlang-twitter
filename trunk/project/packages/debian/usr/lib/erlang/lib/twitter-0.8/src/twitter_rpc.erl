2746437f9e40c9f6fde0e477fd8b52b3b57df77a
jeanlou.dupont <jeanlou.dupont@72dbc67c-803b-11de-8058-effe40bf0168>
2009-09-14 14:32:24 +0000

    release 0.8

---
+++ trunk/project/packages/debian/usr/lib/erlang/lib/twitter-0.8/src/twitter_rpc.erl
@@ -0,0 +1,97 @@
+%% Author: Jean-Lou Dupont
+%% Created: 2009-08-21
+%% Description: 
+-module(twitter_rpc).
+-compile(export_all).
+
+%%
+%% MACROS
+%%
+-define(SUPPORTED_CMDS, [getapiversion, reload, status, getcmds]).
+-define(API_VERSION, {1,0}).
+-define(SERVER,     twitter).
+-define(TIMEOUT,    1000).
+-define(SWITCH,     twitter_hwswitch).
+
+
+getcmds() ->
+	?SUPPORTED_CMDS.
+
+%% ----------------------              ------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%% RPC handling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ----------------------              ------------------------------
+
+%% Retrieves the Pid of the daemon
+%%
+handle_rpc(ReplyTo, _FromNode, RC, status) ->
+	rpc_reply(ReplyTo, {RC, os:getpid()});
+
+%% Retrieves the supported commands through the RPC interface
+%%
+handle_rpc(ReplyTo, _FromNode, RC, getcmds) ->
+	rpc_reply(ReplyTo, {RC, ?SUPPORTED_CMDS});
+
+
+%% Reloads the configuration from file
+%%
+handle_rpc(ReplyTo, _FromNode, RC, reload) ->
+	?SWITCH:publish(sys, reload),
+	rpc_reply(ReplyTo, {RC, ok});
+
+
+%% Retrieves the API version
+%%
+handle_rpc(ReplyTo, _FromNode, RC, getapiversion) ->
+	rpc_reply(ReplyTo, {RC, ?API_VERSION});
+
+
+
+
+
+%% CATCH-ALL
+%%
+handle_rpc(ReplyTo, _, _, _) ->
+	rpc_reply(ReplyTo, {error, invalid_request}).
+
+
+%% Validates if the RPC Command is supported
+%%
+rpc_validate_command(Command) when is_atom(Command) ->
+	lists:member(Command, ?SUPPORTED_CMDS);
+
+rpc_validate_command(_) ->
+	invalid.
+
+
+%% Replies to an RPC call.
+%% The recipient loop of the message normally sits
+%% in the code of the function 'rpc'.
+%%
+rpc_reply(ReplyTo, Message) ->
+	ReplyTo ! {rpc_reply, Message}.
+
+	
+%% ----------------------                 ------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ----------------------                 ------------------------------
+
+
+%% @private
+rpc(ReplyContext, Q) ->
+	FromNode=node(),
+	%%io:format("call: from[~p] Q[~p]~n", [FromNode, Q]),
+	%%?TOOLS:msg("rpc: From[~p] Message[~p]", [FromNode, Q]),
+	?SERVER ! {rpc, self(), {FromNode, ReplyContext, Q}},
+	receive
+		{rpc_reply, Reply} ->
+			Reply;
+	
+		Other ->
+			error_logger:error_msg("~p rpc: received [~p]~n", [?MODULE, Other]),
+			{error, rpcerror}
+	
+	after ?TIMEOUT ->
+			error_logger:error_msg("~p: rpc: timeout~n", [?MODULE]),
+			{error, rpc_timeout}
+	end.
+
